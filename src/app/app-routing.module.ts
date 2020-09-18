d7c4af902a319df67d2c839f140bb5cda29b59ff
Dan GR <drk25@njit.edu>
2020-09-18 18:52:14 -0400

    routes added in first dropdown

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ContentComponent } from '../app/components/content/content.component';
+import { EducationComponent } from '../app/components/education/education.component';
+import { AboutUsComponent } from '../app/components/about-us/about-us.component'
+
+
+
+
+const routes: Routes = [
+  { path: 'content', component: ContentComponent},
+  { path: 'education', component: EducationComponent},
+  { path: 'aboutus', component: AboutUsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [ContentComponent, EducationComponent, AboutUsComponent]
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {MatMenuTrigger} from '@angular/material';
-
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,16 +8,20 @@ import {MatMenuTrigger} from '@angular/material';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  
   @ViewChild(MatMenuTrigger, {static: false}) trigger: MatMenuTrigger;
   recheckIfInMenu: boolean;
-  constructor() { }
+  constructor(private router: Router){}
   
   ngOnInit() {
   }
   openResourceMenu() {
     this.trigger.openMenu();
   }
+  onClick(){
+    this.router.navigate(['/education']);
 
+  }
   closeResourceMenu() {
     setTimeout(() => {
       if (this.recheckIfInMenu === false) {
